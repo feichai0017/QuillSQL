b09caf38e43df8151475a3f0ee20fc03d47a4715
feichai0017 <songguocheng348@gmail.com>
2025-11-10 17:31:28 +1100

    refactor: move heap related file into heap package

--- src/storage/heap/mod.rs
+++ src/storage/heap/mod.rs
@@ -1,1 +1,7 @@
+pub mod heap_recovery;
+pub mod mvcc_heap;
+pub mod table_heap;
 pub mod wal_codec;
+
+pub use mvcc_heap::MvccHeap;
+pub use table_heap::{TableHeap, TableIterator};
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -3,15 +3,17 @@ pub mod disk_manager;
 pub mod disk_scheduler;
 pub mod engine;
 pub mod heap;
-pub mod heap_recovery;
 pub mod index;
 pub mod io;
-pub mod mvcc_heap;
 pub mod page;
-pub mod table_heap;
 pub mod tuple;
+
 pub use engine::{
     DefaultStorageEngine, IndexHandle, IndexScanRequest, ScanOptions, StorageEngine, TableHandle,
     TupleStream,
 };
-pub use mvcc_heap::MvccHeap;
+
+pub use heap::mvcc_heap::{self, MvccHeap};
+pub use heap::heap_recovery;
+pub use heap::table_heap;
+pub use heap::table_heap::{TableHeap, TableIterator};
